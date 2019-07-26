06c2ef30edc3c9ba98656bcdd638167f1fbe9a04
Aprilighost666 <53329627+Aprilighost666@users.noreply.github.com>
2019-07-26 13:41:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-tes
+<head> <title>Hacked By Phantomghost</title>
+<center>
+<Body bgcolor="black">
+<font color="green">
+<center>
+<font size="3" face="Andalus"> <font color="red"><H2>#PRAYFORNEWZEALAND #NEWZEALANDSHOOTING <br>
+Fuck Fraser Anning, an Australian Senator Blames Muslims For New Zealand Mosque Terror Attack</H2><br>
+<img src="https://pbs.twimg.com/media/D1siXL0X0AA_xW4.jpg" width="420px"><p>
+<font color="green">
+Mass shootings at mosques in Christchurch, New Zealand, And as usual, if terrorists are not Muslim, they don't call them terrorists, only brutal shooters<br>
+May Allah grant all those who died in the New Zealand Mosque Shooting to be granted Jannah insha’Allah.<p>
+<font color="red"> 
+<p>Hacked by 4prili666h05T - PhantomGhost 
+<br><font color="red">
+./4prili666h05T - ./Mr.3RR0R - ./Mr.Aljabar - ./Exect1337 - ./Z3U54774CK - ./N1C3x13 - ./6hosthere502 - ./L4663R666H05T - ./D34D 5L00P - ./IF22 - ./213_90N6
+<iframe width="0%" height="0" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/332066223&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+<!-- Hacked By Phantomghost -->
